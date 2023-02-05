18bf7b8f515e0b52acdd6d0bcd795ba81e1d2915
Roberto  Varela <robertovarela@Robertos-MBP.lan>
2023-02-05 10:04:42 -0600

    Added particles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import { Typography } from '@mui/material';
 import Page from '@/src/components/shared/page';
+import PARTICLES_JSON from '@/public/assets/particles-config.json';
 
-const Home = () => (
-  <Page>
-    <Typography variant='h1'>Roberto Varela</Typography>
-  </Page>
-);
+const Home = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <Page>
+      <Particles id='tsparticles' init={particlesInit} options={PARTICLES_JSON} />
+      <Typography variant='h1'>Roberto Varela</Typography>
+    </Page>
+  );
+};
 
 export default Home;
