db996dec984653da043e4de645574b9b096e52a5
Roberto  Varela <robertovarela@Robertos-MBP.lan>
2023-02-05 17:42:03 -0600

    Moved particles to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 // @vendors
-import React from 'react';
+import React, { useCallback } from 'react';
 import { CacheProvider } from '@emotion/react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
 // @components
 import Nav from '@/src/components/shared/nav';
+
+// @public
+import PARTICLES_JSON from '@/public/assets/particles-config.json';
 
 // @utility
 import createEmotionCache from '@/utility/createEmotionCache';
@@ -15,15 +20,22 @@ import '@/styles/globals.css';
 
 const clientSideEmotionCache = createEmotionCache();
 
-const App = ({ Component, emotionCache = clientSideEmotionCache, pageProps }) => (
-  <CacheProvider value={emotionCache}>
-    <ThemeProvider theme={mainTheme}>
-      <CssBaseline />
-      <Nav />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </CacheProvider>
-);
+const App = ({ Component, emotionCache = clientSideEmotionCache, pageProps }) => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={mainTheme}>
+        <CssBaseline />
+        <Particles id='tsparticles' init={particlesInit} options={PARTICLES_JSON} />
+        <Nav />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
 
 App.defaultProps = {
   emotionCache: undefined,
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,17 @@
 // @vendors
-import React, { useCallback } from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
+import React from 'react';
 import { Typography } from '@mui/material';
 
 // @components
 import Page from '@/src/components/shared/page';
 
-// @public
-import PARTICLES_JSON from '@/public/assets/particles-config.json';
-
-const Home = () => {
-  const particlesInit = useCallback(async (engine) => {
-    await loadFull(engine);
-  }, []);
-
-  return (
-    <Page className='home-page'>
-      <Particles id='tsparticles' init={particlesInit} options={PARTICLES_JSON} />
-      <div className='home-page-content'>
-        <Typography className='home-page-content_name' variant='h2' variantMapping={{ h2: 'h1' }}>Roberto Varela</Typography>
-        <Typography className='home-page-content_title' paragraph>FRONT-END ENGINEER</Typography>
-      </div>
-    </Page>
-  );
-};
+const Home = () => (
+  <Page className='home-page'>
+    <div className='home-page-content'>
+      <Typography className='home-page-content_name' variant='h2' variantMapping={{ h2: 'h1' }}>Roberto Varela</Typography>
+      <Typography className='home-page-content_title' paragraph>FRONT-END ENGINEER</Typography>
+    </div>
+  </Page>
+);
 
 export default Home;
