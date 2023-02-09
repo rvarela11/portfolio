2780fc4d14d6abad040a5c420966b5387b5f9aa8
Roberto  Varela <robertovarela@Robertos-MBP.lan>
2023-02-08 22:44:40 -0600

    Add bio on home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,6 +10,11 @@ const Home = () => (
     <div className='home-page-content'>
       <Typography className='home-page-content_name' variant='h2' variantMapping={{ h2: 'h1' }}>Roberto Varela</Typography>
       <Typography className='home-page-content_title' paragraph>FRONT-END ENGINEER</Typography>
+      <div className='home-page-content_bio'>
+        <Typography paragraph>
+          7+ years of front-end development experience with a passion for UI/UX, scaling platforms, and creating intuitive and dynamic user experiences.
+        </Typography>
+      </div>
     </div>
   </Page>
 );
