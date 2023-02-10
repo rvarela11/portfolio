c7ad4105b3376ec00a376f7999d64a24e845b876
Roberto  Varela <robertovarela@Robertos-MBP.lan>
2023-02-09 20:25:03 -0600

    fix - /silvercar route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/silvercar',
+        destination: 'https://rvarela11.github.io/silvercar',
+        permanent: false,
+      },
+    ];
+  },
   reactStrictMode: true,
 };
 
