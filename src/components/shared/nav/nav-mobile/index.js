60bbd7b2ee1264a772ad7f88f84bb041df8de52d
Roberto  Varela <robertovarela@Robertos-MBP.lan>
2023-02-09 20:06:37 -0600

    fix: Disabled nav mobile ripple

--- src/components/shared/nav/nav-mobile/index.js
+++ src/components/shared/nav/nav-mobile/index.js
@@ -24,6 +24,7 @@ const NavMobile = () => {
     <nav className='nav nav-mobile'>
       <IconButton
         color='inherit'
+        disableRipple
         onClick={handleDrawerOpen}
       >
         <MenuIcon />
