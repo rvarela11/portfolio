222516bb7f44ce3c1daefa473d6cecbe156f2f8c
Roberto  Varela <robertovarela@Robertos-MBP.lan>
2023-02-10 20:25:14 -0600

    Removed NavMobile

--- src/components/shared/nav/index.js
+++ src/components/shared/nav/index.js
@@ -1,24 +1,13 @@
 // @vendors
 import React from 'react';
-import { useMediaQuery, useTheme } from '@mui/material';
 
 // @components
 import NavLinks from './nav-links';
-import NavMobile from './nav-mobile';
 
-const Nav = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-
-  if (isMobile) {
-    return <NavMobile />;
-  }
-
-  return (
-    <nav className='nav'>
-      <NavLinks />
-    </nav>
-  );
-};
+const Nav = () => (
+  <nav className='nav'>
+    <NavLinks />
+  </nav>
+);
 
 export default Nav;
